6d23b8bbe8a13f8d51086b2bba8aadf46264c7f6
Xadjayevv23 <xadjayev1919@gmail.com>
2022-04-06 16:51:45 +0500

    added models

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,7 @@ class BlogPostModel(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('title'))
     description = models.TextField(verbose_name=_('description'))
     main_image = models.ImageField(upload_to='blog-post-img/', verbose_name=_('main imag'))
-    banner = models.ImageField(upload_to='blog-pos-img/', verbose_name=_('banner'))
+    banner = models.ImageField(upload_to='blog-post-img/', verbose_name=_('banner'))
     author = models.ForeignKey(AuthorModel, on_delete=models.RESTRICT, related_name='posts', verbose_name=_('author'))
     tags = models.ManyToManyField(BlogTagModel, related_name='posts', verbose_name=_('tags'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at'))
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import BlogPostModel
 
 
 class BlogListView(ListView):
-    pass
+    model = BlogPostModel
+    template_name = 'main/blog_list.html'
+    context_object_name = 'posts'
